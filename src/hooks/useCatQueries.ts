b85b9c0202bd9eed7136903b55f6ac02db745d1f
Inna Mykytiuk <innka-pinnka@ukr.net>
2024-11-01 18:29:19 +0200

    trying to add base url

--- src/hooks/useCatQueries.ts
+++ src/hooks/useCatQueries.ts
@@ -3,13 +3,14 @@ import axios from "axios";
 import { Breed, CatImage } from "../types/catTypes.ts";
 
 // const API_KEY = import.meta.env.VITE_CAT_API_KEY;
+const BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 export const useBreeds = () => {
   return useQuery<Breed[], Error>(
     "breeds",
     async () => {
       const response = await axios.get<Breed[]>(
-        `https://api.thecatapi.com/v1/breeds?api_key=live_cdh15RXPzJfwEjosvt3aBjDvy064dn7ALDvHxW3ioJmMgZSkRgbG5KUIeo427OB3`
+        `${BASE_URL}/breeds?api_key=live_cdh15RXPzJfwEjosvt3aBjDvy064dn7ALDvHxW3ioJmMgZSkRgbG5KUIeo427OB3`
       );
       return response.data;
     },
@@ -28,7 +29,7 @@ export const useCatImages = (
     ["catImages", selectedBreed, page],
     async () => {
       const response = await axios.get<CatImage[]>(
-        `https://api.thecatapi.com/v1/images/search?limit=${limit}&breed_ids=${selectedBreed}&api_key=live_cdh15RXPzJfwEjosvt3aBjDvy064dn7ALDvHxW3ioJmMgZSkRgbG5KUIeo427OB3&page=${page}`
+        `${BASE_URL}/images/search?limit=${limit}&breed_ids=${selectedBreed}&api_key=live_cdh15RXPzJfwEjosvt3aBjDvy064dn7ALDvHxW3ioJmMgZSkRgbG5KUIeo427OB3&page=${page}`
       );
       return response.data;
     },
