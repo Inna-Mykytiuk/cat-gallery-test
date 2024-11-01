220a02c19941c94421b56c2906f25b89763573fa
Inna Mykytiuk <innka-pinnka@ukr.net>
2024-11-01 18:33:19 +0200

    bug with env

--- src/hooks/useCatQueries.ts
+++ src/hooks/useCatQueries.ts
@@ -2,15 +2,14 @@ import { useQuery } from "react-query";
 import axios from "axios";
 import { Breed, CatImage } from "../types/catTypes.ts";
 
-// const API_KEY = import.meta.env.VITE_CAT_API_KEY;
-const BASE_URL = import.meta.env.VITE_API_BASE_URL;
-
 export const useBreeds = () => {
   return useQuery<Breed[], Error>(
     "breeds",
     async () => {
       const response = await axios.get<Breed[]>(
-        `${BASE_URL}/breeds?api_key=live_cdh15RXPzJfwEjosvt3aBjDvy064dn7ALDvHxW3ioJmMgZSkRgbG5KUIeo427OB3`
+        `${
+          import.meta.env.VITE_API_BASE_URL
+        }/breeds?api_key=live_cdh15RXPzJfwEjosvt3aBjDvy064dn7ALDvHxW3ioJmMgZSkRgbG5KUIeo427OB3`
       );
       return response.data;
     },
@@ -29,7 +28,9 @@ export const useCatImages = (
     ["catImages", selectedBreed, page],
     async () => {
       const response = await axios.get<CatImage[]>(
-        `${BASE_URL}/images/search?limit=${limit}&breed_ids=${selectedBreed}&api_key=live_cdh15RXPzJfwEjosvt3aBjDvy064dn7ALDvHxW3ioJmMgZSkRgbG5KUIeo427OB3&page=${page}`
+        `${
+          import.meta.env.VITE_API_BASE_URL
+        }/images/search?limit=${limit}&breed_ids=${selectedBreed}&api_key=live_cdh15RXPzJfwEjosvt3aBjDvy064dn7ALDvHxW3ioJmMgZSkRgbG5KUIeo427OB3&page=${page}`
       );
       return response.data;
     },
