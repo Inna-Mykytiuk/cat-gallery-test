1b068fe78c57a1c6a9fa2edbdc4b115df4cba104
Inna Mykytiuk <innka-pinnka@ukr.net>
2024-11-01 18:12:34 +0200

    d13

--- src/hooks/useCatQueries.ts
+++ src/hooks/useCatQueries.ts
@@ -2,21 +2,19 @@ import { useQuery } from "react-query";
 import axios from "axios";
 import { Breed, CatImage } from "../types/catTypes.ts";
 
-// Зчитування значень з .env
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
-const CAT_API_KEY = import.meta.env.VITE_CAT_API_KEY;
-
 export const useBreeds = () => {
   return useQuery<Breed[], Error>(
     "breeds",
     async () => {
       const response = await axios.get<Breed[]>(
-        `${API_BASE_URL}/breeds?api_key=${CAT_API_KEY}`
+        `${import.meta.env.VITE_API_BASE_URL}/breeds?api_key=${
+          import.meta.env.VITE_CAT_API_KEY
+        }`
       );
       return response.data;
     },
     {
-      staleTime: 600000, // Кешування на 10 хвилин
+      staleTime: 600000,
     }
   );
 };
@@ -30,13 +28,17 @@ export const useCatImages = (
     ["catImages", selectedBreed, page],
     async () => {
       const response = await axios.get<CatImage[]>(
-        `${API_BASE_URL}/images/search?limit=${limit}&breed_ids=${selectedBreed}&api_key=${CAT_API_KEY}&page=${page}`
+        `${
+          import.meta.env.VITE_API_BASE_URL
+        }/images/search?limit=${limit}&breed_ids=${selectedBreed}&api_key=${
+          import.meta.env.VITE_CAT_API_KEY
+        }&page=${page}`
       );
       return response.data;
     },
     {
-      keepPreviousData: true, // Зберігає попередні дані
-      staleTime: 300000, // Кешування на 5 хвилин
+      keepPreviousData: true,
+      staleTime: 300000,
     }
   );
 };
