4a6b57bc42b117295cd72788d585b3e09debffd3
Inna Mykytiuk <innka-pinnka@ukr.net>
2024-11-01 16:45:43 +0200

    dep3

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,18 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
-import CatGallery from './pages/CatGallery';
-import FavoriteCats from './pages/FavoriteCats';
-import Layout from './components/Layout';
+import Test from './components/Test';
+// import CatGallery from './pages/CatGallery';
+// import FavoriteCats from './pages/FavoriteCats';
+// import Layout from './components/Layout';
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Layout />}>
+      <Route path="/" element={<Test />} />
+      {/* <Route path="/" element={<Layout />}>
         <Route index element={<CatGallery />} />
         <Route path="favorites" element={<FavoriteCats />} />
-      </Route>
+      </Route> */}
     </Routes>
   );
 }
---
+++ src/components/Test.tsx
@@ -0,0 +1,11 @@
+
+
+const Test = () => {
+  return (
+    <div>
+      TEST
+    </div>
+  )
+}
+
+export default Test
