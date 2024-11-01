90279cd18a748c055b128da0ee27a44854870d13
Inna Mykytiuk <innka-pinnka@ukr.net>
2024-11-01 17:55:40 +0200

    d12

--- src/hooks/useCatQueries.ts
+++ src/hooks/useCatQueries.ts
@@ -2,14 +2,14 @@ import { useQuery } from "react-query";
 import axios from "axios";
 import { Breed, CatImage } from "../types/catTypes.ts";
 
-const API_KEY = import.meta.env.VITE_CAT_API_KEY;
+// const API_KEY = import.meta.env.VITE_CAT_API_KEY;
 
 export const useBreeds = () => {
   return useQuery<Breed[], Error>(
     "breeds",
     async () => {
       const response = await axios.get<Breed[]>(
-        `https://api.thecatapi.com/v1/breeds?api_key=${API_KEY}`
+        `https://api.thecatapi.com/v1/breeds?api_key=live_cdh15RXPzJfwEjosvt3aBjDvy064dn7ALDvHxW3ioJmMgZSkRgbG5KUIeo427OB3`
       );
       return response.data;
     },
@@ -28,7 +28,7 @@ export const useCatImages = (
     ["catImages", selectedBreed, page],
     async () => {
       const response = await axios.get<CatImage[]>(
-        `https://api.thecatapi.com/v1/images/search?limit=${limit}&breed_ids=${selectedBreed}&api_key=${API_KEY}&page=${page}`
+        `https://api.thecatapi.com/v1/images/search?limit=${limit}&breed_ids=${selectedBreed}&api_key=live_cdh15RXPzJfwEjosvt3aBjDvy064dn7ALDvHxW3ioJmMgZSkRgbG5KUIeo427OB3&page=${page}`
       );
       return response.data;
     },
