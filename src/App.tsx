f9c630392ecaf5e09558838bb4ffbea80d12668f
Inna Mykytiuk <innka-pinnka@ukr.net>
2024-11-01 16:35:06 +0200

    deploy2

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ function App() {
     <Routes>
       <Route path="/" element={<Layout />}>
         <Route index element={<CatGallery />} />
-        <Route path="/favorites" element={<FavoriteCats />} />
+        <Route path="favorites" element={<FavoriteCats />} />
       </Route>
     </Routes>
   );
