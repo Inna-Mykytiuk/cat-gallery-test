102f6ae5ff0703b8f7776f94b46fcea6afe37944
Inna Mykytiuk <innka-pinnka@ukr.net>
2024-11-01 18:42:38 +0200

    last try

--- src/hooks/useCatQueries.ts
+++ src/hooks/useCatQueries.ts
@@ -2,14 +2,15 @@ import { useQuery } from "react-query";
 import axios from "axios";
 import { Breed, CatImage } from "../types/catTypes.ts";
 
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+const API_KEY = import.meta.env.VITE_CAT_API_KEY;
+
 export const useBreeds = () => {
   return useQuery<Breed[], Error>(
     "breeds",
     async () => {
       const response = await axios.get<Breed[]>(
-        `${
-          import.meta.env.VITE_API_BASE_URL
-        }/breeds?api_key=live_cdh15RXPzJfwEjosvt3aBjDvy064dn7ALDvHxW3ioJmMgZSkRgbG5KUIeo427OB3`
+        `${API_BASE_URL}/breeds?api_key=${API_KEY}`
       );
       return response.data;
     },
@@ -28,9 +29,7 @@ export const useCatImages = (
     ["catImages", selectedBreed, page],
     async () => {
       const response = await axios.get<CatImage[]>(
-        `${
-          import.meta.env.VITE_API_BASE_URL
-        }/images/search?limit=${limit}&breed_ids=${selectedBreed}&api_key=live_cdh15RXPzJfwEjosvt3aBjDvy064dn7ALDvHxW3ioJmMgZSkRgbG5KUIeo427OB3&page=${page}`
+        `${API_BASE_URL}/images/search?limit=${limit}&breed_ids=${selectedBreed}&api_key=${API_KEY}&page=${page}`
       );
       return response.data;
     },
