56c67f9a458035a1527c95d4b568e0d1bcf7096b
Inna Mykytiuk <innka-pinnka@ukr.net>
2024-11-03 15:12:07 +0200

    minor changes

--- src/components/BreedFilter.tsx
+++ src/components/BreedFilter.tsx
@@ -25,7 +25,7 @@ const BreedFilter: React.FC<BreedFilterProps> = ({
         }}
         className="rounded-md border border-gray-300 px-4 py-2 shadow-input focus:border-mainBcg focus:outline-none"
       >
-        <option value="">All Breeds</option>
+        <option value="">All Cats</option>
         {breeds.map((breed) => (
           <option key={breed.id} value={breed.id}>
             {breed.name}
--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { defineConfig } from "vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/cat-gallery-test",
+  base: "/cat-gallery-test/",
   resolve: {
     alias: {
       "@": path.resolve("./src"),
